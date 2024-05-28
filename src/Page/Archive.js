a2d60947a480e89f7172ab6001a02669fa5944ac
Houcine Hassine <houcine.hassine@gmail.com>
2024-05-28 22:55:30 +0200

    Create Archive.js

---
+++ src/Page/Archive.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Archive() {
+  return (
+    <div>
+      <h1>Archiv Money</h1>
+      <p>Details about archive...</p>
+    </div>
+  );
+}
+
+export default Archive;
